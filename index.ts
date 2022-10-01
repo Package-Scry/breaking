08695dc40a2b3e97ca6a09e5e82abb1c03c52d81
kristofszollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-10-01 16:53:24 +0200

    call function

--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ import { getChangeLog } from "./getChangeLog"
 export const getChangeLogs = async (
   packageNames: { name: string; currentVersion: string }[]
 ) => {
-  packageNames.forEach(async (npmPackage) => {
+  packageNames.forEach(async npmPackage => {
     getChangeLog(npmPackage)
   })
 
   return { wasSuccessful: true }
 }
+
+getChangeLogs([{ name: "json2csv", currentVersion: "1.0.0" }])

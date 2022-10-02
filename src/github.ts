69368c05b57616388eea17a278e9bd76110ffaee
kristofszollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-10-03 01:02:05 +0200

    use PAT

--- src/github.ts
+++ src/github.ts
@@ -1,26 +1,18 @@
-import { createAppAuth } from "@octokit/auth-app"
 import { request } from "@octokit/request"
 
 export const getRelease = async (owner: string, repo: string, tag: string) => {
   try {
     console.log("requesting", owner, repo, tag)
 
-    const auth = createAppAuth({
-      appId: process.env.APP_ID,
-      privateKey: process.env.PRIVATE_KEY,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
+    const requestWithAuth = request.defaults({
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
     })
 
-    // Retrieve JSON Web Token (JWT) to authenticate as app
-    const { token } = await auth({ type: "app" })
-
-    const result = await request(
+    const result = await requestWithAuth(
       "GET /repos/{owner}/{repo}/releases/tags/{tag}",
       {
-        headers: {
-          authorization: token,
-        },
         owner,
         repo,
         tag,

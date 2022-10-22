93414af76dd102704caaf0e89d94ddcb86b89725
kristofszollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-10-22 22:20:52 +0200

    escape regex characters

--- src/getBreakingChange.ts
+++ src/getBreakingChange.ts
@@ -1,6 +1,6 @@
 import { marked } from "marked"
 import { REG_X_HEADER } from "./constants.js"
-import { isTheSameHeader } from "./utils.js"
+import { escapeRegExp, isTheSameHeader } from "./utils.js"
 
 export const getBreakingChange = (changeLog: string): string | null => {
   const headers: string[] = changeLog.match(REG_X_HEADER) ?? [""]
@@ -12,14 +12,18 @@ export const getBreakingChange = (changeLog: string): string | null => {
   if (breakingChangeIndex === -1) return fallbackText
 
   const headerCount = (
-    headers[breakingChangeIndex].match(new RegExp("#", "g")) || []
+    headers[breakingChangeIndex].match(new RegExp("#", "g")) ?? []
   ).length
+
   const nextHeader = headers
     .slice(breakingChangeIndex + 1)
     .find(header => isTheSameHeader(header, headerCount))
   const isLastItem = !nextHeader
-  const start = changeLog.search(headers[breakingChangeIndex])
-  const end = isLastItem ? changeLog.length : changeLog.search(nextHeader)
+  const start = changeLog.search(escapeRegExp(headers[breakingChangeIndex]))
+  const end = isLastItem
+    ? changeLog.length
+    : changeLog.search(escapeRegExp(nextHeader))
+
   const breakingText = changeLog.slice(start, end)
 
   return breakingText ? marked.parse(breakingText) : fallbackText

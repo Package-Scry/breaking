b0fbe3de2ddaa58e448912ac8698fe312768100e
kristofszollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-10-23 17:41:28 +0200

    test(getMajorChangelog): files with inconsistent headers

--- test/getChangeLogFromFile/index.test.ts
+++ test/getChangeLogFromFile/index.test.ts
@@ -1,7 +1,10 @@
 import { expect, test } from "vitest"
 import path from "path"
 import fs from "fs"
-import { getMajorVersionHeaders } from "../../src/getChangeLogFromFile"
+import {
+  getMajorChangeLogs,
+  getMajorVersionHeaders,
+} from "../../src/getChangeLogFromFile"
 import { REG_X_HEADER } from "../../src/constants"
 
 const json2csvMajorVersionHeaders = [
@@ -17,6 +20,67 @@ const json2csvMajorVersionHeaders = [
 const mongoMajorVersionHeaders = [
   "## [3.0.0] - 2019-06-17",
   "## [2.0.0] - 2017-10-09",
+]
+
+const mongoMajorChangeLogs = [
+  {
+    version: "3.0.0",
+    changes: {
+      breaking:
+        '<h3 id="breaking-changes"><strong>BREAKING CHANGES</strong></h3>\n' +
+        "<ul>\n" +
+        "<li>Drop Node.js 4 &amp; 6 support</li>\n" +
+        "<li>Upgrade <code>mongoose</code> to v5 and <code>mongodb</code> to v3 and drop old version support</li>\n" +
+        "<li>Replace deprecated mongo operation</li>\n" +
+        "<li>MongoStore need to supply client/clientPromise instead of db/dbPromise due to depns upgrade</li>\n" +
+        "</ul>\n" +
+        '<h2 id="added">Added</h2>\n' +
+        "<ul>\n" +
+        "<li>Add Node.js 10 &amp; 12 support</li>\n" +
+        "<li>Implement store.all function (#291)</li>\n" +
+        "<li>Add option <code>writeOperationOptions</code> (#295)</li>\n" +
+        "<li>Add Transparent crypto support (#314)</li>\n" +
+        "</ul>\n" +
+        '<h2 id="changed">Changed</h2>\n' +
+        "<ul>\n" +
+        "<li>Change test framework from Mocha to Jest</li>\n" +
+        "<li>Change linter from <code>xo</code> to <code>eslint</code></li>\n" +
+        "</ul>\n" +
+        '<h2 id="203---2018-12-03">[2.0.3] - 2018-12-03</h2>\n' +
+        '<h2 id="fixed">Fixed</h2>\n' +
+        "<ul>\n" +
+        "<li>Fixed interval autoremove mode to use current date with every interval (#304, #305) (jlampise)</li>\n" +
+        "</ul>\n" +
+        '<h2 id="202---2018-11-20">[2.0.2] - 2018-11-20</h2>\n' +
+        '<h2 id="fixed-1">Fixed</h2>\n' +
+        "<ul>\n" +
+        "<li>Fxi #300 DeprecationWarning: collection.remove is deprecated. Use deleteOne, deleteMany, or bulkWrite instead</li>\n" +
+        "<li>Fxi #297 DeprecationWarning: collection.update is deprecated. Use updateOne, updateMany, or bulkWrite instead</li>\n" +
+        "</ul>\n" +
+        '<h2 id="201---2018-01-04">[2.0.1] - 2018-01-04</h2>\n' +
+        '<h2 id="fixed-2">Fixed</h2>\n' +
+        "<ul>\n" +
+        "<li>Fixed #271 TypeError: cb is not a function (brainthinks)</li>\n" +
+        "</ul>\n",
+    },
+  },
+  {
+    version: "2.0.0",
+    changes: {
+      breaking:
+        '<h3 id="breaking-changes"><strong>BREAKING CHANGES</strong></h3>\n' +
+        "<ul>\n" +
+        "<li><strong>Drop</strong> Node.js 0.12 and io.js support</li>\n" +
+        "<li><strong>Drop</strong> MongoDB 2.x support</li>\n" +
+        "<li><strong>Drop</strong> mongodb driver &lt; 2.0.36 support</li>\n" +
+        "<li><strong>Drop</strong> mongoose &lt; 4.1.2 support</li>\n" +
+        "</ul>\n" +
+        '<h2 id="changed">Changed</h2>\n' +
+        "<ul>\n" +
+        "<li><strong>Fix</strong> <code>ensureIndex</code> deprecation warning ([</li>\n" +
+        "</ul>\n",
+    },
+  },
 ]
 
 test("getting major headers in a file with consistent header types", () => {
@@ -35,4 +99,15 @@ test("getting major headers in a file with inconsistent header types", () => {
   const majorVersionHeaders = getMajorVersionHeaders(headers)
 
   expect(majorVersionHeaders).toEqual(mongoMajorVersionHeaders)
+})
+
+test("getting major change logs in a file with inconsistent header types", () => {
+  const uriRaw = path.join(__dirname, "mongoRaw.txt")
+  const changesRaw = fs.readFileSync(uriRaw, "utf-8").toString()
+  const headers: string[] = changesRaw?.match(REG_X_HEADER) ?? []
+  const majorVersionHeaders = getMajorVersionHeaders(headers)
+  console.log("BEGIN")
+  const majorChangeLogs = getMajorChangeLogs(changesRaw, majorVersionHeaders)
+
+  expect(majorChangeLogs).toStrictEqual(mongoMajorChangeLogs)
 })

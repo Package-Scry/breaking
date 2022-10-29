8da686526d3c20c88338d20c6976cd29985776e3
kristofszollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-10-29 21:16:59 +0200

    fix: changelog test

--- test/getChangeLogs/index.test.ts
+++ test/getChangeLogs/index.test.ts
@@ -2,13 +2,14 @@ import { assert, expect, test } from "vitest"
 import { reactChangeLogsV18 } from "./mocks"
 import { getChangeLog } from "../../src/getChangeLog"
 
-test("getting change logs (for react 15-latest)", async () => {
-  const changeLogs = await getChangeLog({
-    name: "react",
-    currentVersion: "15.0.0",
-  })
-
-  console.log(changeLogs[0])
+test("getting change logs (for react 15-18)", async () => {
+  // Testing only first three entries
+  const changeLogs = (
+    await getChangeLog({
+      name: "react",
+      currentVersion: "15.0.0",
+    })
+  ).slice(0, 3)
 
   expect(changeLogs).toStrictEqual(reactChangeLogsV18)
 

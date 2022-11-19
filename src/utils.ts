1b2bd95a01bb95aa2dadc4979b3e902ea8990744
kristofszollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-11-19 20:43:41 +0100

    refact: use npm api

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,6 @@
-import util from "util"
-import { exec } from "child_process"
 import findVersions from "find-versions"
 import { REG_X_HEADER } from "./constants.js"
 import fetch from "node-fetch"
-
-const pExec = util.promisify(exec)
 
 export const getMajorVersion = (text: string): number =>
   parseInt(findVersions(text, { loose: true })?.[0].split(".")[0] ?? "", 10)
@@ -13,7 +9,6 @@ export const getLatestMajorVersion = async (name: string) => {
   const url = `https://registry.npmjs.org/${name}`
 
   try {
-    // console.log("fetching file from", url)
     const response = await fetch(url)
     const data: unknown = await response.json()
     const latestVersion = parseInt(
@@ -32,19 +27,22 @@ export const isTheSameHeader = (header: string, headerCount: number) =>
   (header.match(new RegExp("#", "g")) || []).length === headerCount
 
 export const getGitHubRepoUrl = async (name: string) => {
-  const { stdout, stderr } = await pExec(
-    `npm repo ${name} --browser=false --json`
-  )
+  const url = `https://registry.npmjs.org/${name}`
 
-  if (stderr) {
-    console.error(`------- ${name} -------`)
-    console.error(stderr)
+  try {
+    const response = await fetch(url)
+    const data: any = await response.json()
+    const repoUrl = data?.repository?.url
+      ?.replace("git+", "")
+      ?.replace(".git", "")
+
+    return { wasSuccessful: !!repoUrl, url: repoUrl }
+  } catch (error) {
+    console.error(error)
+    return null
   }
-
-  const url = JSON.parse(stdout)?.url.split("/").slice(0, 5).join("/")
-
-  return { wasSuccessful: !!stdout, url }
 }
+
 export const getHeaders = (changeLog: string) =>
   changeLog.match(REG_X_HEADER) ?? [""]
 
